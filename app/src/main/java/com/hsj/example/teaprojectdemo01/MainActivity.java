dbd3d75dc200f4eaa949e3633e0e8dd735088602
hsjwcf <hsjwcf@163.com>
2016-07-24 21:11:16 +0800

    实现了打开详情Activity的功能

--- app/src/main/java/com/hsj/example/teaprojectdemo01/MainActivity.java
+++ app/src/main/java/com/hsj/example/teaprojectdemo01/MainActivity.java
@@ -1,9 +1,9 @@
 package com.hsj.example.teaprojectdemo01;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,25 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * 打开详情Activity
+     * @param view
+     */
+    public void openDetail(View view){
+        Intent intent=new Intent(this,DetailActivity.class);
+        startActivity(intent);
     }
 }
